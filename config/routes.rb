a27a56fcc45faa4a90ce335ca94c1cf2f1ebb4cd
David Koch <davidckoch@gmail.com>
2016-05-12 15:01:13 -0500

    Chapter 5 exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
   root 'static_pages#home'
-  #get "users/new"
   get  '/signup',  to: 'users#new'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
